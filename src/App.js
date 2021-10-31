2476292f89eee7cf5db11e82a1565497a80cdeef
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-10-31 07:16:52 -0300

    Adicionando Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from './components/Header';
+
+
 function App() {
   return (
-    <h1>Começando o projeto</h1>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="nav">
+        <div>
+          <a href="/">Rick and Morty App</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
